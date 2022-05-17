4b7b75c6e9be86c8919d7e004275c9f7d51de90e
PradyumnaGarg <pradumnagarg@hotmail.com>
2022-05-17 20:46:28 +0530

    add features - signup login

--- src/features/authentication/login/Login.tsx
+++ src/features/authentication/login/Login.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function Login() {
-  return (<div>Hi</div>);
+  return (
+    <div className="flex h-full w-full justify-center items-center">
+      <form className="bg-white rounded-3xl p-8 space-y-4 flex flex-col">
+        <div className="flex flex-col space-y-2">
+          <label htmlFor="email">
+            Email
+            <input type="email" name="email" id="email" />
+          </label>
+        </div>
+      </form>
+
+    </div>
+  );
 }
 
 export default Login;
